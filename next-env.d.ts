3150c0430cfe7fa1aa08623685a01f019fe75aef
Adam Szwaba <szwaba.adam@gmail.com>
2020-07-30 20:39:39 +0200

    Add and test mdx integration

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,10 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+declare module '*.md' {
+  const MDXComponent: (props: any) => JSX.Element;
+  export default MDXComponent;
+}
+declare module '*.mdx' {
+  const MDXComponent: (props: any) => JSX.Element;
+  export default MDXComponent;
+}
