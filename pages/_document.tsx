87067af128470855d5e94b1c6e222f4a98e6c678
Adam Szwaba <szwaba.adam@gmail.com>
2020-08-05 11:25:09 +0200

    Funkys tuff

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import React from 'react';
-import { ServerStyleSheets } from '@material-ui/core/styles';
+// import { ServerStyleSheets } from '@material-ui/core/styles';
 
 class MyDocument extends Document {
   render() {
